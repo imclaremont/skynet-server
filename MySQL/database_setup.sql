3fcd6d95a3c25a3fb068233daf693460e8eab258
imclaremont <149583958+imclaremont@users.noreply.github.com>
2024-10-17 16:39:28 +0900

    Update database_setup.sql

--- MySQL/database_setup.sql
+++ MySQL/database_setup.sql
@@ -1,1 +1,15 @@
+CREATE DATABASE IF NOT EXISTS flaskdb;
+USE flaskdb;
 
+-- 테이블 생성
+SOURCE create_tables/drone_table.sql;
+SOURCE create_tables/station_table.sql;
+SOURCE create_tables/deliveries_table.sql;
+SOURCE create_tables/no_fly_zones_table.sql;
+SOURCE create_tables/weather_table.sql;
+SOURCE create_tables/path_between_station_table.sql;
+SOURCE create_tables/intersection_table.sql;
+SOURCE create_tables/delivery_path_table.sql;
+
+-- 외래 키 설정
+SET FOREIGN_KEY_CHECKS = 1;
