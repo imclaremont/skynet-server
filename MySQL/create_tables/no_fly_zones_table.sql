09c72f59bcdf7d6c5f807b00d4ae72f324efd356
imclaremont <149583958+imclaremont@users.noreply.github.com>
2024-10-17 16:36:31 +0900

    Update no_fly_zones_table.sql

--- MySQL/create_tables/no_fly_zones_table.sql
+++ MySQL/create_tables/no_fly_zones_table.sql
@@ -1,1 +1,8 @@
-
+CREATE TABLE no_fly_zones_table (
+    zone_id VARCHAR(36) NOT NULL COMMENT 'PK',
+    zone_name VARCHAR(100) NOT NULL,
+    longitude DECIMAL(9, 6) NOT NULL COMMENT 'XXX.XXXXXX°',
+    latitude DECIMAL(9, 6) NOT NULL COMMENT 'XXX.XXXXXX°',
+    radius INT NOT NULL COMMENT '[m]',
+    PRIMARY KEY (zone_id)
+);
