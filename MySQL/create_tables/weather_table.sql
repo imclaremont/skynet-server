d85d092b7fc657fc74883623d74b739a82aaf049
imclaremont <149583958+imclaremont@users.noreply.github.com>
2024-10-17 16:36:46 +0900

    Update weather_table.sql

--- MySQL/create_tables/weather_table.sql
+++ MySQL/create_tables/weather_table.sql
@@ -1,1 +1,7 @@
-
+CREATE TABLE weather_table (
+    weather_id VARCHAR(36) NOT NULL COMMENT 'PK',
+    wind_speed FLOAT NULL DEFAULT 0,
+    rainfall FLOAT NULL DEFAULT 0,
+    observation_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (weather_id)
+);
