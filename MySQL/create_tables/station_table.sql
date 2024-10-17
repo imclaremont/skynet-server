7c9ac3dab42f2dbaa52e6cd716adfe09957ad39a
imclaremont <149583958+imclaremont@users.noreply.github.com>
2024-10-17 16:34:35 +0900

    Update station_table.sql

--- MySQL/create_tables/station_table.sql
+++ MySQL/create_tables/station_table.sql
@@ -1,1 +1,8 @@
-
+CREATE TABLE station_table (
+    station_id VARCHAR(36) NOT NULL COMMENT 'PK',
+    station_name VARCHAR(100) NOT NULL,
+    station_longitude DECIMAL(9, 6) NOT NULL COMMENT 'XXX.XXXXXX°',
+    station_latitude DECIMAL(9, 6) NOT NULL COMMENT 'XXX.XXXXXX°',
+    capacity INT NOT NULL,
+    PRIMARY KEY (station_id)
+);
