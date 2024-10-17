5627326aec0ba20610a34473ee13386146d35ab4
imclaremont <149583958+imclaremont@users.noreply.github.com>
2024-10-17 16:35:24 +0900

    Update path_between_station_table.sql

--- MySQL/create_tables/path_between_station_table.sql
+++ MySQL/create_tables/path_between_station_table.sql
@@ -1,1 +1,9 @@
-
+CREATE TABLE path_between_station_table (
+    path_id VARCHAR(36) NOT NULL COMMENT 'PK',
+    origin_station_id VARCHAR(36) NOT NULL COMMENT 'FK',
+    destination_station_id VARCHAR(36) NOT NULL COMMENT 'FK',
+    distance INT NOT NULL COMMENT 'm',
+    PRIMARY KEY (path_id),
+    FOREIGN KEY (origin_station_id) REFERENCES station_table(station_id),
+    FOREIGN KEY (destination_station_id) REFERENCES station_table(station_id)
+);
