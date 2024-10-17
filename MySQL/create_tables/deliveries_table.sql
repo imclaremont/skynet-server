21daa9b1269cfb53ebd02306d94a68cb371ef3e7
imclaremont <149583958+imclaremont@users.noreply.github.com>
2024-10-17 16:34:07 +0900

    Update deliveries_table.sql

--- MySQL/create_tables/deliveries_table.sql
+++ MySQL/create_tables/deliveries_table.sql
@@ -1,1 +1,11 @@
-
+CREATE TABLE deliveries_table (
+    delivery_id VARCHAR(36) NOT NULL COMMENT 'PK',
+    origin_station_id VARCHAR(36) NOT NULL COMMENT 'FK',
+    destination_station_id VARCHAR(36) NOT NULL COMMENT 'FK',
+    delivery_status VARCHAR(100) NOT NULL DEFAULT 'pending',
+    entire_arrival_time TIMESTAMP NULL,
+    arrival_time TIMESTAMP NULL,
+    PRIMARY KEY (delivery_id),
+    FOREIGN KEY (origin_station_id) REFERENCES station_table(station_id),
+    FOREIGN KEY (destination_station_id) REFERENCES station_table(station_id)
+);
