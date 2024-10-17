2b57fdc050d8d95dfbd30eea89e926f73a0dd251
imclaremont <149583958+imclaremont@users.noreply.github.com>
2024-10-17 16:33:41 +0900

    Update drone_table.sql

--- MySQL/create_tables/drone_table.sql
+++ MySQL/create_tables/drone_table.sql
@@ -1,1 +1,11 @@
-
+CREATE TABLE drone_table (
+    drone_id VARCHAR(36) NOT NULL COMMENT 'PK',
+    delivery_id VARCHAR(36) NOT NULL COMMENT 'FK',
+    drone_status ENUM('available', 'in-flight', 'maintenance') NOT NULL DEFAULT 'available',
+    longitude DECIMAL(9, 6) NOT NULL COMMENT 'XXX.XXXXXX°',
+    latitude DECIMAL(9, 6) NOT NULL COMMENT 'XXX.XXXXXX°',
+    battery INT NOT NULL DEFAULT 100 COMMENT '0% ~ 100%',
+    last_update_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (drone_id),
+    FOREIGN KEY (delivery_id) REFERENCES deliveries_table(delivery_id)
+);
