6921b5434a7ffdb87d27e6ce430297e05f6d5138
imclaremont <149583958+imclaremont@users.noreply.github.com>
2024-10-17 16:35:40 +0900

    Update intersection_table.sql

--- MySQL/create_tables/intersection_table.sql
+++ MySQL/create_tables/intersection_table.sql
@@ -1,1 +1,10 @@
-
+CREATE TABLE intersection_table (
+    intersection_id VARCHAR(36) NOT NULL COMMENT 'PK',
+    path1_id VARCHAR(36) NOT NULL COMMENT 'FK',
+    path2_id VARCHAR(36) NOT NULL COMMENT 'FK',
+    longitude DECIMAL(9, 6) NOT NULL COMMENT 'XXX.XXXXXX°',
+    latitude DECIMAL(9, 6) NOT NULL COMMENT 'XXX.XXXXXX°',
+    PRIMARY KEY (intersection_id),
+    FOREIGN KEY (path1_id) REFERENCES path_between_station_table(path_id),
+    FOREIGN KEY (path2_id) REFERENCES path_between_station_table(path_id)
+);
